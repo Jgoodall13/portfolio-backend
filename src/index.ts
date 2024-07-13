070ffe8871a6ffaf14b2f06d512cb2a19dd9b613
Jacob Goodall <jraygoodall@gmail.com>
2024-07-13 13:56:09 -0700

    connected

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,58 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import nodemailer from "nodemailer";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-// Use CORS middleware
 app.use(cors());
+app.use(express.json()); // to parse JSON bodies
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
+});
+
+// Configure nodemailer
+const transporter = nodemailer.createTransport({
+  secure: false,
+  port: 587,
+  host: "live.smtp.mailtrap.io",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+app.post("/send-email", (req: Request, res: Response) => {
+  const { to, subject, text } = req.body;
+
+  console.log(to, subject, text);
+  //   const mailOptions = {
+  //     from: process.env.EMAIL_USER,
+  //     to,
+  //     subject,
+  //     text,
+  //   };
+
+  //   transporter.sendMail(mailOptions, (error, info) => {
+  //     if (error) {
+  //       console.log(error);
+  //       res.status(500).send("Failed to send email");
+  //     } else {
+  //       console.log("Email sent: " + info.response);
+  //       res.status(200).send("Email sent successfully");
+  //     }
+  //   });
+  let returnData = [
+    {
+      from: "Jacob Goodall",
+      status: 200,
+    },
+  ];
+  res.status(200).send(returnData);
 });
 
 app.listen(port, () => {
