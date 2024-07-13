8b1c710b114a2ce6ea951e4bd0cfc52d83f8140e
Jacob Goodall <jraygoodall@gmail.com>
2024-07-13 12:51:32 -0700

    setting up cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-// src/index.ts
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+// Use CORS middleware
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
